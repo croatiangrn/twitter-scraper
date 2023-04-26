f35c0821d26bd2c8f00bddb750847685d706ceaa
Marko Mikulić <mikulic.marko996@gmail.com>
2023-04-26 16:46:53 +0200

    Log

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
@@ -35,6 +36,7 @@ func (s *Scraper) RequestAPI(req *http.Request, target interface{}) error {
 
 	for _, cookie := range s.client.Jar.Cookies(req.URL) {
 		if cookie.Name == "ct0" {
+			log.Println(cookie.Value)
 			req.Header.Set("X-CSRF-Token", cookie.Value)
 			break
 		}
